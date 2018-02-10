fd6207c10bc20f5dfe8dfee49d5fc7f708ccc25d
Johan Louwers <johan.louwers@capgemini.com>
2018-02-10 14:37:17 +0100

    changed stub location
    Changes stub for openalerts.json in openAlerts.js to a github location

--- js/viewModels/common/openAlerts.js
+++ js/viewModels/common/openAlerts.js
@@ -2,7 +2,7 @@ define(['ojs/ojcore', 'knockout'], function (oj, ko) {
 
     function OpenAlertsViewModule() {
         var self = this;
-        var urlOpenAlerts = "http://192.168.56.53/fraudapp/openalerts.json";
+        var urlOpenAlerts = "https://raw.githubusercontent.com/louwersj/Oracle-JET-testapp-1/master/openalerts.json";
 
         self.createOpenAlertsModel = function () {
             var OpenAlerts = oj.Model.extend({
